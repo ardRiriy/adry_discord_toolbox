55f30ae8d9d075a37cf237cea415ad14b787cd7c
ardririy <hynnkgw27@gmail.com>
2024-01-20 05:19:18 +0900

    ping

---
+++ src/adry_discord_toolbox/main.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from discord import app_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print("logged in")
+    activity_message = "このbotはテスト運用されています"
+    await client.change_presence(activity=discord.Game(activity_message))
+    await tree.sync()
+
+@tree.command(name='ping', description="応答確認")
+async def ping_pong(interaction: discord.Interaction):
+    await interaction.response.send_message("pong")
+
+
+client.run(os.environ["DISCORD_TOKEN"])
