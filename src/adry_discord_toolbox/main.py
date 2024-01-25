ae50e546c931ac2e643583206316da432d3ae682
ardririy <hynnkgw27@gmail.com>
2024-01-26 05:37:17 +0900

    予定の追加・削除ができるように

--- src/adry_discord_toolbox/main.py
+++ src/adry_discord_toolbox/main.py
@@ -1,12 +1,13 @@
 import os
+import datetime
 
 import discord
-from discord import app_commands
+from discord import *
 from dotenv import load_dotenv
 
 from google.oauth2 import service_account
-from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 load_dotenv()
 
@@ -19,7 +20,8 @@ tree = app_commands.CommandTree(client)
 creds = service_account.Credentials.from_service_account_file('discord-bot-key.json')
 service = build('calendar', 'v3', credentials=creds)
 
-calendar_id = 'discord-bot@discord-bot-411721.iam.gserviceaccount.com'
+calendar_id = 'hynnkgw2525@gmail.com'
+
 
 @client.event
 async def on_ready():
@@ -36,5 +38,29 @@ async def ping_pong(interaction: discord.Interaction):
 async def deploy(interaction: discord.Interaction):
     await interaction.response.send_message("再起動します")
     os.system('sh update.sh')
+
+@tree.command(name='add', description="Googleカレンダーの操作")
+async def add(interaction: discord.Interaction):
+    start_time = datetime.datetime(2024, 1, 1, 9, 0, 0)
+    end_time = datetime.datetime(2024, 1, 1, 10, 0, 0)
+    event = {
+        'summary': 'test',
+        'start': {
+            'dateTime': start_time.strftime('%Y-%m-%dT%H:%M:%S'),
+            'timeZone': 'Asia/Tokyo',
+        },
+        'end': {
+            'dateTime': end_time.strftime('%Y-%m-%dT%H:%M:%S'),
+            'timeZone': 'Asia/Tokyo',
+        },
+    }
+    try:
+        res = service.events().insert(calendarId=calendar_id, body=event).execute()
+        normal_message = f'予定を作成しました！: [URL]({res.get("htmlLink")})'
+        await interaction.response.send_message(normal_message)
+    except HttpError as error:
+        error_message = f'An error occurred: {error}'
+        await interaction.response.send_message(error_message)
+
 
 client.run(os.environ["DISCORD_TOKEN"])
